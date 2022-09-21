41d8b377fb6243835189e451d216b1186584f332
devmarcioreis <107413382+devmarcioreis@users.noreply.github.com>
2022-09-21 11:04:09 -0300

    Main js navbar

---
+++ assets/js/main.js
@@ -0,0 +1,20 @@
+const header = document.querySelector('.navbar')
+const openNavBtn = document.querySelector('#nav-open')
+const closeNavBtn = document.querySelector('#nav-close')
+const navLink = document.querySelector('.navbar')
+
+const openNav = () => {
+    header.style.display = 'flex'
+    openNavBtn.style.display = 'none'
+    closeNavBtn.style.display = 'inline-block'
+}
+openNavBtn.addEventListener('click', openNav)
+
+const closeNav = () => {
+    header.style.display = 'none'
+    openNavBtn.style.display = 'inline-block'
+    closeNavBtn.style.display = 'none'
+}
+closeNavBtn.addEventListener('click', closeNav)
+
+navLink.addEventListener('click', closeNav)
